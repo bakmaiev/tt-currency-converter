f64c55941b597d9b6dabe425214927614cd73a19
bakmaiev <bakmaiev@gmail.com>
2023-12-02 22:20:57 +0200

    Create Header with basic logic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchData } from "../../api/fetchCurrencies";
+import { StyledHeader, StyledRatesWrapper } from "./Header.styled";
 
 const Header = () => {
-  return <header></header>;
+  const [usdToUah, setUsdToUah] = useState(null);
+  const [eurToUah, setEurToUah] = useState(null);
+
+  useEffect(() => {
+    const fetchActualData = async () => {
+      try {
+        const usdResponce = await fetchData("USD", "UAH");
+        setUsdToUah(usdResponce);
+
+        const eurToUah = await fetchData("EUR", "UAH");
+        setEurToUah(eurToUah);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    fetchActualData();
+  }, []);
+
+  return (
+    <StyledHeader>
+      <h1>Currency Converter</h1>
+      <StyledRatesWrapper>
+        <span>USD to UAH: {usdToUah}</span>
+        <span>EUR to UAH: {eurToUah}</span>
+      </StyledRatesWrapper>
+    </StyledHeader>
+  );
 };
 
 export default Header;
---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 16px;
+  border: 1px solid hsl(0, 0%, 80%);
+`;
+
+export const StyledRatesWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
